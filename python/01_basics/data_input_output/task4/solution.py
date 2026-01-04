8e8e0192812473df2920734508b51aab84aaf7eb
kovbasaaaka <yevseevtrofim@gmail.com>
2026-01-04 18:04:17 +0500

    Update solution.py

--- python/01_basics/data_input_output/task4/solution.py
+++ python/01_basics/data_input_output/task4/solution.py
@@ -1,1 +1,6 @@
-
+number = int(input())
+first_number = number // 1000
+second_number = (number % 1000) // 100
+third_number = (number % 100) // 10
+fourth_number = number % 10
+print(f"Цифра в позиции тысяч равна {first_number}\nЦифра в позиции сотен равна {second_number}\nЦифра в позиции десятков равна {third_number}\nЦифра в позиции единиц равна {fourth_number}")
