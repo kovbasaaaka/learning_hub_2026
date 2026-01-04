ec9be6ebe170c0579dfd7461fe1507b3f13f125e
kovbasaaaka <yevseevtrofim@gmail.com>
2026-01-04 17:42:32 +0500

    Update solution.py

--- python/01_basics/data_input_output/task2/solution.py
+++ python/01_basics/data_input_output/task2/solution.py
@@ -1,1 +1,3 @@
-
+a1, d, n = int(input()), int(input()), int(input()) # Ввод значений
+ny_member = a1 + d * (n - 1)                        # Формула и вычисление n - члена арифметической прогрессии
+print(ny_member)                                    # Вывод данных
